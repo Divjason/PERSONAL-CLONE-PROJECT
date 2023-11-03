7b3b2a2c48426fb68312f86da30755f2757466c1
Divjason <sejinpark@tcocs.com>
2023-11-03 18:33:18 +0900

    231103

--- 012_cyworld_htmlcssjs/jukeboxstyle.js
+++ 012_cyworld_htmlcssjs/jukeboxstyle.js
@@ -1,5 +1,20 @@
-const song = document.querySelectorAll(".album_table_song")[0];
-const play = song.querySelector("i");
+const songs = document.querySelectorAll(".album_table_song");
+console.log(songs);
+let i = 0;
+for (let el of songs) {
+  let play = el.querySelector(".fa-caret-right");
+  let pause = el.querySelector(".fa-pause");
+  play.addEventListener("click", (e) => {
+    e.currentTarget.closest("td").querySelector("audio").play();
+  });
+  pause.addEventListener("click", (e) => {
+    e.currentTarget.closest("td").querySelector("audio").pause();
+  });
+}
+
+/*
+const play = songs.querySelector("i");
 play.addEventListener("click", (e) => {
   e.currentTarget.closest("td").querySelector("audio").play();
 });
+*/
